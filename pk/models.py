1311e45badd385001960eabae1baf8cac9bb9a37
chandu4442 <43944925+chandu4442@users.noreply.github.com>
2018-11-29 20:21:00 +0530

    Update models.py

--- pk/models.py
+++ pk/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Cricket(models.Model):
     runs=models.IntegerField(max_length=100)
-    avg=models.SmallIntegerField(max_length=10)
+    avg=models.SmallIntegerField(max_length=100)
 
 
 
